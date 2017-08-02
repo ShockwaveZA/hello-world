dd5e9b7e8dae18d676e25fc36c29ca4b80f7620f
ShockwaveZA <minal.pramlall@yahoo.com>
2017-08-02 08:14:04 +0200

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -11,27 +11,26 @@ CREATE DATABASE prac2
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
-CREATE TABLE Student (
-    sNumber varchar(6) PRIMARY KEY,
-    sFullNames varchar(255),
-    sDateOfBirth date,
-    sDegreeCode varchar(255),
-    sYearOfStudy int,
-    FOREIGN KEY (sDegreeCode) REFERENCES DegreeProgram(dCode)
-);
-
 CREATE TABLE Undergraduate (
    	uNumber varchar(6) PRIMARY KEY,
+    uFullNames varchar(255),
+    uDateOfBirth date,
+    uDegreeCode varchar(255),
+    uYearOfStudy int,
     uCourseRegistration varchar(255),
-    FOREIGN KEY (uNumber) REFERENCES Student(sNumber)
+    FOREIGN KEY (uDegreeCode) REFERENCES DegreeProgram(dCode)
 );
 
 
 CREATE TABLE Postgraduate (
     pNumber varchar(6) PRIMARY KEY,
+    pFullNames varchar(255),
+    pDateOfBirth date,
+    pDegreeCode varchar(255),
+    pYearOfStudy int,
     pCategory varchar(255),
     pSupervisor varchar(255),
-    FOREIGN KEY (pNumber) REFERENCES Student(sNumber)
+    FOREIGN KEY (pDegreeCode) REFERENCES DegreeProgram(dCode)
 );
 
 CREATE TABLE DegreeProgram (
@@ -47,3 +46,24 @@ CREATE TABLE Course (
     cDepartment varchar(255),
     cCredits int
 );
+
+INSERT INTO DegreeProgram VALUES
+    ("BSc", "Bachelor of Science", 3, "EBIT"),
+    ("BIT", "Bachelor of IT", 4, "EBIT"),
+    ("PhD", "Philosophiae Doctor", 5, "EBIT");
+    
+INSERT INTO Course VALUES
+    ("COS301", "Software Engineering", "Computer Science", 40),
+    ("COS326", "Database Systems", "Computer Science", 20),
+    ("MTH301", "Discrete Mathematics", "Mathematics", 15),
+    ("PHL301", "Logical Reasoning", "Philosophy", 15);
+    
+INSERT INTO Undergraduate VALUES
+    ("140010", "Mr Minal Pramlall", "10-01-1996", "BSc", 3, "COS301,COS326,MTH301"),
+    ("140015", "Ms Leandra Methven", "25-05-1995", "BSc", 3, "COS301,PHL301,MTH301"),
+    ("131120", "Mr Kurt Lourens", "30-01-1995", "BIT", 3, "COS301,COS326,PHL301"),
+    ("131140", "Mr Keith Kinsey", "20-02-1996", "BIT", 4, "COS301,COS326,MTH301,PHL301");
+     
+INSERT INTO Postgraduate VALUES
+    ("101122", "Mr Tristan Bowman", "15-06-1987", "PhD", 2, "full time", "Mr Emilio Singh"),
+    ("121101", "Mr Jason van Eck", "27-04-1985", "PhD", 3, "part time", "Mr Emilio Singh");
