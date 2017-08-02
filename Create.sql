9dffc0b8a0dce04515d4b9db185b8a60d19ab0bb
ShockwaveZA <minal.pramlall@yahoo.com>
2017-08-02 09:13:40 +0200

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -48,22 +48,22 @@ CREATE TABLE Course (
 );
 
 INSERT INTO DegreeProgram VALUES
-    ("BSc", "Bachelor of Science", 3, "EBIT"),
-    ("BIT", "Bachelor of IT", 4, "EBIT"),
-    ("PhD", "Philosophiae Doctor", 5, "EBIT");
+    ('BSc', 'Bachelor of Science', 3, 'EBIT'),
+    ('BIT', 'Bachelor of IT', 4, 'EBIT'),
+    ('PhD', 'Philosophiae Doctor', 5, 'EBIT');
     
 INSERT INTO Course VALUES
-    ("COS301", "Software Engineering", "Computer Science", 40),
-    ("COS326", "Database Systems", "Computer Science", 20),
-    ("MTH301", "Discrete Mathematics", "Mathematics", 15),
-    ("PHL301", "Logical Reasoning", "Philosophy", 15);
+    ('COS301', 'Software Engineering', 'Computer Science', 40),
+    ('COS326', 'Database Systems', 'Computer Science', 20),
+    ('MTH301', 'Discrete Mathematics', 'Mathematics', 15),
+    ('PHL301', 'Logical Reasoning', 'Philosophy', 15);
     
 INSERT INTO Undergraduate VALUES
-    ("140010", "Mr Minal Pramlall", "10-01-1996", "BSc", 3, "COS301,COS326,MTH301"),
-    ("140015", "Ms Leandra Methven", "25-05-1995", "BSc", 3, "COS301,PHL301,MTH301"),
-    ("131120", "Mr Kurt Lourens", "30-01-1995", "BIT", 3, "COS301,COS326,PHL301"),
-    ("131140", "Mr Keith Kinsey", "20-02-1996", "BIT", 4, "COS301,COS326,MTH301,PHL301");
+    ('140010', 'Mr Minal Pramlall', '10-01-1996', 'BSc', 3, 'COS301,COS326,MTH301'),
+    ('140015', 'Ms Leandra Methven', '25-05-1995', 'BSc', 3, 'COS301,PHL301,MTH301'),
+    ('131120', 'Mr Kurt Lourens', '30-01-1995', 'BIT', 3, 'COS301,COS326,PHL301'),
+    ('131140', 'Mr Keith Kinsey', '20-02-1996', 'BIT', 4, 'COS301,COS326,MTH301,PHL301');
      
 INSERT INTO Postgraduate VALUES
-    ("101122", "Mr Tristan Bowman", "15-06-1987", "PhD", 2, "full time", "Mr Emilio Singh"),
-    ("121101", "Mr Jason van Eck", "27-04-1985", "PhD", 3, "part time", "Mr Emilio Singh");
+    ('101122', 'Mr Tristan Bowman', '15-06-1987', 'PhD', 2, 'full time', 'Mr Emilio Singh'),
+    ('121101', 'Mr Jason van Eck', '27-04-1985', 'PhD', 3, 'part time', 'Mr Emilio Singh');
