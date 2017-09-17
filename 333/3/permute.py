32e9d634f6ef6782bda024e7d71c0a836510288f
ShockwaveZA <minal.pramlall@yahoo.com>
2017-09-17 14:28:33 +0200

    Create permute.py

---
+++ 333/3/permute.py
@@ -0,0 +1,72 @@
+import sys
+import os
+import pickle
+
+def generatePermutations(arr=[1, 2, 3]):
+	out = []
+	return permute(arr, out)
+
+def permute(arr, out):
+	if (len(arr) == 1):
+		newOut = []
+		newOut.extend(out)
+		newOut.extend(arr)
+		return newOut
+	ret = []
+	for k in range(0, len(arr)):
+		newArr = [];
+		for j in range(0, len(arr)):
+			if (j != k):
+				newArr.append(arr[j])
+		newOut = [];
+		newOut.extend(out)
+		newOut.append(arr[k])
+		ret.extend(permute(newArr, newOut))
+	return ret
+
+def modifyPermutations(arr):
+	for k in range(0, len(arr)):
+		arr[k] = arr[k] ** 2
+
+def picklePermutations(perms, fname = 'permutations.pkl'):
+	with open(fname, 'wb') as f:
+		pickle.dump(perms, f)
+
+
+def getPickledPermutations(fname = 'permutations.pkl'):
+	with open(fname, 'rb') as f:
+		return pickle.load(f)
+
+def printList(arr, length):
+	count = 0
+	for k in range(0, len(arr)):
+		if (count < length - 1):
+			print (arr[k], ' ', end='')
+			count += 1
+		else:
+			print (arr[k])
+			count = 0
+
+args = []
+for k in range(1, len(sys.argv)):
+	args.append(int(sys.argv[k]))
+	
+args = [3, 4, 5]
+yn = input('Read previous permutations? (y/n): ')
+if (yn == 'y'):
+	perms = []
+	fname = input('file name: ')
+	if (fname == ''):
+		perms = getPickledPermutations()
+	else:
+		perms = getPickledPermutations(fname)
+	printList(modifyPermutations(perms), len(args))
+else:
+	perms = []
+	if (args == []):
+		perms = generatePermutations()
+	else:
+		perms = generatePermutations(args)
+	printList(perms, len(args))
+	picklePermutations(perms)
+	
